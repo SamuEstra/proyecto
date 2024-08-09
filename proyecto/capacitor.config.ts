9654ec980cec031f2f15698dca7de4e91c172356
wirisoft <wirideveloper@gmail.com>
2024-08-08 19:44:21 -0600

    configuracion http

--- proyecto/capacitor.config.ts
+++ proyecto/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Calculadora',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  server: {
+    cleartext: true,
+    androidScheme: "http"
+  }
 };
-
 export default config;
